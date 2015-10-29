5cd2c0ae4641edb65ba88f4c7bf0e0a49a23063a
brian <obcomology@gmail.com>
2015-10-29 11:07:42 +0100

    corrected black depth info for base cases

--- src/Chap03/Exercise09.hs
+++ src/Chap03/Exercise09.hs
@@ -5,8 +5,8 @@ import Chap03.Data.RedBlackTree
 fromOrdList :: Ord a => [a] -> RedBlackTree a
 fromOrdList xs =
    let go :: Int -> [a] -> (RedBlackTree a, Int, [a])
-       go 0 xs'   = (E, 1, xs')
-       go 1 (x:xs')  = (T B E x E, 2, xs')
+       go 0 xs'   = (E, -1, xs')
+       go 1 (x:xs')  = (T B E x E, 0, xs')
        go len xs' = 
          let mid = len `div` 2
              (p, q) = (mid, len - mid - 1)
